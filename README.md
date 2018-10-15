06d60907c8bd1a1a13b39a30e349b473c564bf43
Lizh <lizh@LizhdeiMac.local>
2018-10-15 20:25:22 +0800

    update REDEME.md

---
+++ README.md
@@ -0,0 +1,26 @@
+# Electron-TypeScript-React
+
+### Create An Electron App with TypeScript, React, Redux, React-Router
+
+## Installation
+```bash
+$ npm install -g electron-typescript-react
+```
+
+## Usage
+```bash
+$ build-app init <project-name>
+```
+
+#### Example
+```bash
+$ build-app init electron-project
+```
+
+
+## Resources for Learning
+
+- [electronjs.org/docs](https://electronjs.org/docs) - all of Electron's documentation
+- [TypeScript](https://www.typescriptlang.org/)
+- [React](https://reactjs.org/)
+- [Redux](https://redux.js.org/)
--- bin/commander.js
+++ bin/commander.js
@@ -4,16 +4,16 @@ const program = require('commander');
 const packageJson = require('../package.json');
 
 const resolvePath = (relativePath) => path.resolve(__dirname, relativePath);
-const DEFAULT_PROJECT_NAME = 'Electron-typeScript-React';
+const DEFAULT_PROJECT_NAME = 'Electron-TypeScript-React';
 let projectName = DEFAULT_PROJECT_NAME;
 
 program
   .version(packageJson.version)
   .usage('build-app <command> [options]')
-  .option('-v --version', 'output the version number')
   .on('--help', function() {
     console.log('');
     console.log('Examples:');
+    console.log('build-app init Electron-TypeScript-React');
   });
 
   program
